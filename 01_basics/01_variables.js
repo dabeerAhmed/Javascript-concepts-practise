8496a2cc5d5e1c665a46bd2f3a3d6785fa273ff9
DabeerAhmed <dabeerahmed962@gmail.com>
2024-01-29 09:42:56 +0000

    javascript variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345;
+let accountEmail = "dabeer@gmail.com";
+var accountPassword = "12345";
+accountCity = "karachi"
+let accountName;
+
+// accountId = 2
+accountEmail = "db@db.com";
+accountPassword = "212121"
+accountCity = "lahore"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountName])
