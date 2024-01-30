ca6e8b05f877c53eadda3d0a27e55d7fbc95d722
DabeerAhmed <dabeerahmed962@gmail.com>
2024-01-30 09:46:21 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+alert(3+3)  //We are using node
+// Code reusability should be high
+
+//datatypes
+let name = "Dabeer";
+let age = 22;
+let isLoggedIn = false;
+
+// number = 2 to the power 52
+// bigInt = used when a very big number needed to assign suvh as stockmarket/ reddit web etc.
+// string = ""
+// booloean = true/false
+// null
