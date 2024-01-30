cd21940de0ae805495e504cbd595f00d22f3cb11
DabeerAhmed <dabeerahmed962@gmail.com>
2024-01-30 10:21:20 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = null;
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// null => 0
+// undefined => NaN
+// true => 1; false => 0;
+// "33" => 33
+// "33abc" => NaN
+
+isLoggedIn = "dabeer";
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1=>true; 0=>false;
+// ""=> false
+// "dabeer" => true
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
