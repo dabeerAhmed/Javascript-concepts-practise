86bdb0ceeae17e196df6b3f2dcfd3fc62a92a78d
DabeerAhmed <dabeerahmed962@gmail.com>
2024-01-31 12:08:59 +0000

    confusion in operation conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = null;
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 // null => 0
 // undefined => NaN
@@ -15,7 +15,7 @@ console.log(valueInNumber)
 
 isLoggedIn = "dabeer";
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1=>true; 0=>false;
 // ""=> false
@@ -24,5 +24,33 @@ console.log(booleanIsLoggedIn)
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//  ******************************************* Operations **********************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "Hello"
+let str2 = " Dabeer"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log("1"+2+2); 
+// console.log(1+2+"3");
+// console.log((2+5)*(3%6)) // this should the way. parenthesis should be used if you need to write a code like this
+
+// console.log(+true);
+// console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+// console.log(num1, num2, num3)
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter)
