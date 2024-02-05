45180b052d47b9f4344a4ba868a7d9f3a7a171a5
DabeerAhmed <dabeerahmed962@gmail.com>
2024-02-05 10:02:56 +0000

    strings completed

---
+++ 01_basics/05_string.js
@@ -0,0 +1,25 @@
+const name = "Dabeer";
+const repoCount = 50;
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new  String('Dabeer-db');
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('b'))
+// console.log(gameName.toUpperCase());
+const newString = gameName.substring(0,5)
+// console.log(newString)
+const anotherString = gameName.slice(-8,5)
+// console.log(anotherString);
+
+const newStringOne = "        dabeer     "
+// console.log(newStringOne);
+// console.log(newStringOne.trim())
+
+const url = "https://dabeer.com/dabeer%20ahmed"
+// console.log(url.replace('%20','-'));
+// console.log(url.includes('dabeer'));
+// console.log(gameName.split('-'))
