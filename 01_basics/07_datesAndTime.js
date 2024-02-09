4b6382c085ae790284c604034d7cfb19192099fa
DabeerAhmed <dabeerahmed962@gmail.com>
2024-02-09 18:29:11 +0000

    dates and time

---
+++ 01_basics/07_datesAndTime.js
@@ -0,0 +1,25 @@
+// const myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+const myCreatedDate1 = new Date(21, 2, 2023)
+const myCreatedDate2 = new Date(21, 2, 2023, 5, 3)
+const myCreatedDate3 = new Date("2023-01-9")
+// console.log(myCreatedDate3.toLocaleString())
+
+
+const myTimestamp = Date.now()
+// console.log(myTimestamp)
+// console.log(myCreatedDate3.getTime())
+// console.log(Math.floor((Date.now())/1000))
+
+let newDate = new Date();
+// console.log(newDate);
+// console.log(newDate.getDay());
+// console.log(newDate.getMonth()+1);
