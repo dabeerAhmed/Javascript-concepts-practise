0217e625f8251193d2047b872f2483d50a09d3f8
DabeerAhmed <dabeerahmed962@gmail.com>
2024-02-12 12:43:15 +0000

    breif description of arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,17 @@
+// Arrays
+const myArr = [ 0, 1, 2, 3, 4, 5]
+const myHeros = ["Ironman", "Thor", "Spiderman"]
+const myArr2 = new Array(1,2,3,4,5)
+// console.log(myArr[0])
+// console.log(myArr2)
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+// console.log(myArr)
+
+// myArr.unshift(10)   // adds an element at the start of the array
+// myArr.shift()          // removes an element from the start of an array
+// console.log(myArr)
