89ecc03a62ab5114e5ad101ef81d38ac6a94258a
DabeerAhmed <dabeerahmed962@gmail.com>
2024-02-13 12:57:36 +0000

    object basics completed

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -17,3 +17,38 @@ const regularUser = {
     }
 }
 // console.log(regularUser.fullname.userfullname)
+
+const obj1 = {1: "a", 2:"b"}
+const obj2 = {3: "a", 4:"b"}
+const obj4 = {5: "a", 6:"b"}
+
+// const obj3 = {obj1,obj2}
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2, ...obj4}
+// console.log(obj3)
+
+const user = [
+    {
+        id:1,
+        email: "abc@gmail.com"
+    },
+    {
+        id:1,
+        email: "abc@gmail.com"
+    },
+    {
+        id:1,
+        email: "abc@gmail.com"
+    },
+]
+
+// console.log(user[1].email)
+
+
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+// console.log(tinderUser.hasOwnProperty('isLoggedOut'))
