e707b06a768d7a20c5cfd0c9f228e6ec81d1ff39
DabeerAhmed <dabeerahmed962@gmail.com>
2024-02-14 12:04:25 +0000

    functions basics

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -20,5 +20,15 @@ function sayMyName(){
 // function addTwoNumbers(number1, number2){
 //     return number1+number2
 // }
-const result = addTwoNumbers(3,6)
-console.log("result : ", result)
+// const result = addTwoNumbers(3,6)
+// console.log("result : ", result)
+
+function loginUserMsg(username = "abc"){
+    if(!username){
+        console.log("please enter a username")
+        return
+    }
+    return `${username}, just logged in`
+}
+// console.log(loginUserMsg("Dabeer Ahmed"));  //dabeer ahmed just logged in
+// console.log(loginUserMsg());  //undefined just logged in
