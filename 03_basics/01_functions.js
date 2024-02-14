5d4386ccfdacf4c975df9dae852e2cda9f156575
DabeerAhmed <dabeerahmed962@gmail.com>
2024-02-14 10:33:08 +0000

    functions start

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,24 @@
+function sayMyName(){
+    console.log("D")
+    console.log("A")
+    console.log("B")
+    console.log("E")
+    console.log("E")
+    console.log("R")
+}
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2)
+// }
+
+// function addTwoNumbers(number1, number2){
+//     const result = number1 + number2
+//     return result
+// }
+
+// function addTwoNumbers(number1, number2){
+//     return number1+number2
+// }
+const result = addTwoNumbers(3,6)
+console.log("result : ", result)
