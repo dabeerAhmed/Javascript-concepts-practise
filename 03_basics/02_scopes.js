7c3798db5dc93fcd02af4f3e2ad49e6a9351de8e
DabeerAhmed <dabeerahmed962@gmail.com>
2024-02-15 10:03:52 +0000

    scope level and mini hoisting in javascript

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,11 +3,50 @@ let a = 400
 if (true) {
     let a = 10
     const b = 20
-    console.log("Inner : ",a);
+    // console.log("Inner : ",a);
 }
-console.log("Outer : ", a);
+// console.log("Outer : ", a);
 // console.log(b);
 // console.log(c);
 
 
 // interview question : global scope is different in windows and node.
+
+
+function one(){
+    const username = "Dabeer Ahmed"
+
+    function two(){
+        const website = "db-portfolio"
+        // console.log(username)
+    }
+    // console.log(website)
+    two()
+}
+one()
+
+
+if (true) {
+    const username = "Dabeer"
+    if (username === "Dabeer"){
+        const website = " db-port"
+        // console.log(username + website)
+    }
+}
+
+
+
+// +++++++++++++++++++++++++++++ intresting ++++++++++++++++++++++++++++++
+// The below concept is called hoisting in javascript
+
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+
+// console.log(addTwo(5))
+const addTwo = function(num){
+    return num + 2
+}
