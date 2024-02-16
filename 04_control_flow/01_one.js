2ac059c97d69af10e67a0c60fb4a7f7646e0130b
DabeerAhmed <dabeerahmed962@gmail.com>
2024-02-16 13:24:47 +0000

    if else conditional statements

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,46 @@
+const isLoggedIn = true
+const temprature = 44
+
+// <, >, ==, ===, 1=, <=, >=, !=, !==
+
+// if (temprature <= 50) {
+//     console.log("temprature is less than 50")
+// } else{
+//     console.log("temprature is not less than 50")
+// }
+
+// const score=200
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User Power: ${power}`)
+// }
+// console.log(`User Power: ${power}`)
+
+
+//  const balance = 1000
+
+//  if (balance > 500) console.log("test"), console.log("test2");
+
+// if (balance < 500) {
+//     console.log("balance is less than 500")
+// } else if (balance < 750) {
+//     console.log("balance is less than 750");
+// } else if( balance < 900){
+//     console.log("balance is less than 900")
+// } else {
+//     console.log("balance is less than 1200")
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = true
+const loggedInFromEmail = false
+
+// if (userLoggedIn && debitCard && 2===2 && 2==3) {
+//     console.log("user logged in")   
+// }
+
+if (loggedInFromEmail || loggedInFromGoogle) {
+    console.log("user logged in")
+    
+}
