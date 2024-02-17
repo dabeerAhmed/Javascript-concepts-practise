bd68fe8ded03d59581c20dfa9e17c1cefb60cf33
DabeerAhmed <dabeerahmed962@gmail.com>
2024-02-17 19:52:41 +0000

    for each loop completed

---
+++ 05_iterations/05_forEach_loop.js
@@ -0,0 +1,44 @@
+// for Each loop
+
+
+const coding = ["js","ruby","java","python","cpp"]
+
+coding.forEach( function (item){
+    // console.log(item)
+} )
+
+
+coding.forEach( (val) => {
+    // console.log(val)
+} )
+
+
+function printme(item){
+    // console.log(item)
+}
+coding.forEach(printme)
+
+
+coding.forEach( (item, index, arr)=>{
+    // console.log(item, index, arr)
+} )
+
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFile: "js"
+    },
+    {
+        languageName: "java",
+        languageFile: "java"
+    },
+    {
+        languageName: "python",
+        languageFile: "py"
+    },
+]
+
+myCoding.forEach( (item)=>{
+    // console.log(item.languageFile)
+} )
