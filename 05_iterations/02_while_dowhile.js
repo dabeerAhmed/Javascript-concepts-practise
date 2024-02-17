d449b47972f0ad47e41bda743966dade51cce431
DabeerAhmed <dabeerahmed962@gmail.com>
2024-02-17 18:42:17 +0000

    while and do while loop completed

---
+++ 05_iterations/02_while_dowhile.js
@@ -0,0 +1,23 @@
+// while
+
+let index = 0
+while(index <= 10){
+    // console.log(`Value of index is ${index}`)
+    index = index + 2
+}
+
+let myArray = ["flash","superman","batman"]
+let myArr = 0
+while(myArr < myArray.length){
+    // console.log(`Value is ${myArray[myArr]}`)
+    myArr = myArr + 1
+}
+
+
+// do while loop
+
+let score = 1
+do{
+    console.log(`value is ${score}`)
+    score++
+} while (score <= 10)
