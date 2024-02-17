5159a6ce6a20a8ace441f9200233b888953a49f8
DabeerAhmed <dabeerahmed962@gmail.com>
2024-02-17 19:16:24 +0000

    for of loop

---
+++ 05_iterations/03_ArrayLoops.js
@@ -0,0 +1,48 @@
+// for of
+
+//  ["","",""]
+//  [{},{},{}]
+
+const arr = [1,2,3,4,5]
+for (const i of arr) {
+    // console.log(i);   
+}
+
+const greetings = "hello world"
+for(const greet of greetings){
+    if(greet == " "){
+        continue
+    }
+    // console.log(`each character is ${greet}`)
+}
+
+
+// map
+
+const map = new Map()
+map.set('Pk',"Pakistan")
+map.set('In','India')
+map.set('Fr',"France")
+map.set('Pk','Pakistan')
+// console.log(map)
+
+
+for(const key of map){
+    // console.log(key)
+}
+
+
+for(const [key, value] of map){
+    // console.log(key,':-', value)
+}
+
+
+
+const myObj = {
+    game1: "nfs",
+    game2: "spiderman"
+}
+
+// for (const [key,value] of myObj){
+//     console.log(key,value)
+// }
