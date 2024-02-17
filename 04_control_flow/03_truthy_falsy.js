5beb05ed8bedc8d7a898d91ee2be224a6156273b
DabeerAhmed <dabeerahmed962@gmail.com>
2024-02-17 17:32:45 +0000

    truthy and falsy values completed along with ternary operators and nullish coalescing operator

---
+++ 04_control_flow/03_truthy_falsy.js
@@ -0,0 +1,41 @@
+// const userEmail = ""
+
+// if (userEmail){
+//     console.log("got user email");
+// } else {
+//     console.log("dont have user email");
+// }
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// truthy values
+
+// "0", "false", " ", [], {}, function(){}
+
+
+// const arr = []
+// if (arr.length === 0) {
+//     console.log("empty array");  
+// }
+
+// const obj = {}
+// if (Object.keys(obj).length === 0) {
+//     console.log("empty obj");   
+// }
+
+
+// Nullish coalescing operator (??); null, undefined
+
+// let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 10
+// val1 = null ?? 5 ?? 10
+// console.log(val1)
+
+// Ternary operator
+
+// const iceTeaPrice = 100;
+// iceTeaPrice >= 80 ? console.log("is greater than 80") : console.log("less than 80");
