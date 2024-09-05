8618e5221cca5cd3faf6e7658741bf144850f4da
DabeerAhmed <dabeerahmed962@gmail.com>
2024-09-05 19:35:26 +0000

    for loop

--- 05_iterations/01_for_loop.js
+++ 05_iterations/01_for_loop.js
@@ -9,7 +9,6 @@ for (let i = 0; i <= 10; i++) {
     
 }
 
-
 for(let i = 0; i<=10; i++){
     // console.log(`Outer value ${i}`)
     for(let j = 0; j<=10; j++){
@@ -50,10 +49,10 @@ for (let index = 1; index <= 20; index++) {
 
 for (let index = 1; index <= 20; index++) {
     if(index == 5){
-        console.log("5 is the best number")
+        // console.log("5 is the best number")
         continue;
     }
-    console.log(`value of i is ${index}`)
+    // console.log(`value of i is ${index}`)
     
 }
 
