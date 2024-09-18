2977353b1eac8ebf2d612792af1b27f0902ef02c
DabeerAhmed <dabeerahmed962@gmail.com>
2024-09-18 11:05:43 +0000

    async project one completed

---
+++ 08_events/async-project1.js
@@ -0,0 +1,60 @@
+// function randomColor(){
+//     let hex = "0123456789ABCDEF"
+//     let color = "#"
+//     for (let i = 0; i < 6; i++) {
+//         color += hex[Math.floor((Math.random()*16))]
+//     }
+//     return color;
+
+// }
+// // console.log(randomColor())
+
+// let intervalId;
+// const startChangingColor = function(){
+//     if (!intervalId) {
+//         intervalId = setInterval(changeBgColor, 1000)
+//     }
+    
+//     function changeBgColor(){
+//         document.body.style.backgroundColor = randomColor()
+//     }
+// }
+// const stopChangingColor = function(){
+//     clearInterval(intervalId)
+//     intervalId = null
+// }
+
+// document.querySelector("#start").addEventListener('click', startChangingColor)
+// document.querySelector("#stop").addEventListener('click', stopChangingColor)
+
+
+
+
+function randomColor(){
+    let hex = "0123456789ABCDEF"
+    let color = "#"
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor((Math.random()*16))]
+    }
+    return color
+
+}
+// console.log(randomColor())
+let intervalId;
+
+const startChangingColor = function(){
+    function changeBgColor(){
+        document.body.style.backgroundColor = randomColor()
+    }
+    if (!intervalId) {
+        intervalId = setInterval(changeBgColor,500) 
+    }
+}
+
+const stopChangingColor = function(){
+    clearInterval(intervalId)
+    intervalId = null
+}
+document.querySelector("#start").addEventListener('click', startChangingColor)
+
+document.querySelector("#stop").addEventListener('click', stopChangingColor)
