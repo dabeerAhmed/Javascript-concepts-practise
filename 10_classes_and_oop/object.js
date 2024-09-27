075d5f5ca1f3a02abe18fa815b2284977f389507
DabeerAhmed <dabeerahmed962@gmail.com>
2024-09-27 18:54:24 +0000

    prototypes in javascript

--- 10_classes_and_oop/object.js
+++ 10_classes_and_oop/object.js
@@ -5,3 +5,21 @@ multiplyBy5.power = 3
 console.log(multiplyBy5(5))
 console.log(multiplyBy5.power)
 console.log(multiplyBy5.prototype)
+
+
+function createUser(username, score){
+    this.username = username
+    this.score = score
+}
+
+createUser.prototype.increament = function(){
+    console.log(this.score++)
+}
+
+createUser.prototype.print = function(){
+    console.log(`Score is ${this.score}`)
+}
+
+const first = new createUser('dabeer', 45)
+const second = createUser('ahmed', 50)
+first.print()
